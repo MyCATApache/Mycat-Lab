e36d48643156a60afeda7b0b324463ceb94b7066
Benjamin Liu <benjamin.liu@live.cn>
2016-02-19 16:11:25 +0800

    fix a bug that memory be allocate was note signed used and add some unit test

--- Mycat-Memory/src/main/java/io/mycat/memory/ByteBufferPage.java
+++ Mycat-Memory/src/main/java/io/mycat/memory/ByteBufferPage.java
@@ -11,88 +11,93 @@ import sun.nio.ch.DirectBuffer;
  */
 @SuppressWarnings("restriction")
 public class ByteBufferPage {
-	// sun.nio.ch.DirectBuffer
-	private final ByteBuffer buf;
-	private final int chunkSize;
-	private final int chunkCount;
-	private final BitSet chunkAllocateTrack;
-	private final AtomicBoolean allocLockStatus = new AtomicBoolean(false);
-	private final long startAddress;
+    // sun.nio.ch.DirectBuffer
+    private final ByteBuffer buf;
+    private final int chunkSize;
+    private final int chunkCount;
+    private final BitSet chunkAllocateTrack;
+    private final AtomicBoolean allocLockStatus = new AtomicBoolean(false);
+    private final long startAddress;
 
-	public ByteBufferPage(ByteBuffer buf, int chunkSize) {
-		super();
-		this.chunkSize = chunkSize;
-		chunkCount = buf.capacity() / chunkSize;
-		chunkAllocateTrack = new BitSet(chunkCount);
-		this.buf = buf;
-		startAddress = ((sun.nio.ch.DirectBuffer) buf).address();
-	}
+    public ByteBufferPage(ByteBuffer buf, int chunkSize) {
+        super();
+        this.chunkSize = chunkSize;
+        chunkCount = buf.capacity() / chunkSize;
+        chunkAllocateTrack = new BitSet(chunkCount);
+        this.buf = buf;
+        startAddress = ((sun.nio.ch.DirectBuffer) buf).address();
+    }
 
-	public ByteBuffer allocatChunk(int theChunkCount) {
-		if (!allocLockStatus.compareAndSet(false, true)) {
-			return null;
-		}
-		int startChunk = -1;
-		int contiueCount = 0;
-		try {
-			for (int i = 0; i < chunkCount; i++) {
-				if (chunkAllocateTrack.get(i) == false) {
-					if (startChunk == -1) {
-						startChunk = i;
-						contiueCount = 1;
-						if (theChunkCount == 1) {
-							break;
-						}
-					} else {
-						if (++contiueCount == theChunkCount) {
-							break;
-						}
-					}
-				} else {
-					startChunk = -1;
-					contiueCount = 1;
-				}
-			}
-		} finally {
-			allocLockStatus.set(false);
-		}
-		if (contiueCount == theChunkCount) {
-			int offStart = startChunk * chunkSize;
-			int offEnd = offStart + theChunkCount * chunkSize;
-			buf.position(offStart);
-			buf.limit(offEnd);
-			ByteBuffer newBuf = buf.slice();
-			//sun.nio.ch.DirectBuffer theBuf = (DirectBuffer) newBuf;
-			//System.out.println("offAddress " + (theBuf.address() - startAddress));
-			markChunksUsed(startChunk, theChunkCount);
-			return newBuf;
-		} else {
-			//System.out.println("contiueCount " + contiueCount + " theChunkCount " + theChunkCount);
-			return null;
-		}
-	}
+    public ByteBuffer allocatChunk(int theChunkCount) {
+        if (!allocLockStatus.compareAndSet(false, true)) {
+            return null;
+        }
+        int startChunk = -1;
+        int contiueCount = 0;
+        try {
+            for (int i = 0; i < chunkCount; i++) {
+                if (chunkAllocateTrack.get(i) == false) {
+                    if (startChunk == -1) {
+                        startChunk = i;
+                        contiueCount = 1;
+                        if (theChunkCount == 1) {
+                            break;
+                        }
+                    } else {
+                        if (++contiueCount == theChunkCount) {
+                            break;
+                        }
+                    }
+                } else {
+                    startChunk = -1;
+                    contiueCount = 0;
+                }
+            }
+            if (contiueCount == theChunkCount) {
+                int offStart = startChunk * chunkSize;
+                int offEnd = offStart + theChunkCount * chunkSize;
+                buf.position(offStart);
+                buf.limit(offEnd);
+                ByteBuffer newBuf = buf.slice();
+                //sun.nio.ch.DirectBuffer theBuf = (DirectBuffer) newBuf;
+                //System.out.println("offAddress " + (theBuf.address() - startAddress));
+                markChunksUsed(startChunk, theChunkCount);
+                return newBuf;
+            } else {
+                //System.out.println("contiueCount " + contiueCount + " theChunkCount " + theChunkCount);
+                return null;
+            }
+        } finally {
+            allocLockStatus.set(false);
+        }
+    }
 
-	private void markChunksUsed(int startChunk, int theChunkCount) {
-		for (int i = 0; i < theChunkCount; i++) {
-			chunkAllocateTrack.clear(startChunk + i);
-		}
+    private void markChunksUsed(int startChunk, int theChunkCount) {
+        for (int i = 0; i < theChunkCount; i++) {
+            chunkAllocateTrack.set(startChunk + i);
+        }
+    }
 
-	}
+    private void markChunksUnused(int startChunk, int theChunkCount) {
+        for (int i = 0; i < theChunkCount; i++) {
+            chunkAllocateTrack.clear(startChunk + i);
+        }
+    }
 
-	public boolean reclyeBuffer(ByteBuffer parent, int startChunk, int chunkCount) {
+    public boolean recycleBuffer(ByteBuffer parent, int startChunk, int chunkCount) {
 
-		if (parent == this.buf) {
+        if (parent == this.buf) {
 
-			while (!this.allocLockStatus.compareAndSet(false, true)) {
-				Thread.yield();
-			}
-			try {
-				markChunksUsed(startChunk, chunkCount);
-			} finally {
-				allocLockStatus.set(false);
-			}
-			return true;
-		}
-		return false;
-	}
+            while (!this.allocLockStatus.compareAndSet(false, true)) {
+                Thread.yield();
+            }
+            try {
+                markChunksUnused(startChunk, chunkCount);
+            } finally {
+                allocLockStatus.set(false);
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- Mycat-Memory/src/test/java/io/mycat/ipc/TestDirectByteBufferPool.java
+++ Mycat-Memory/src/test/java/io/mycat/ipc/TestDirectByteBufferPool.java
@@ -1,32 +1,122 @@
 package io.mycat.ipc;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 import io.mycat.memory.DirectByteBufferPool;
+import sun.nio.ch.DirectBuffer;
 
 public class TestDirectByteBufferPool {
 
-	@Test
-	public void testAllocate()
-	{
-		int pageSize=1024*1024*100;
-		int allocTimes=1024*1024*100;
-		DirectByteBufferPool pool=new DirectByteBufferPool(pageSize,(short)256,(short) 8);
-		long start=System.currentTimeMillis();
-		for(int i=0;i<allocTimes;i++)
-		{
-			//System.out.println("allocat "+i);
-			//long start=System.nanoTime();
-			int size=i%(1024*8)+1;
-			ByteBuffer byteBufer=pool.allocat(size);
-			//System.out.println("alloc "+size+" usage "+(System.nanoTime()-start));
-			//start=System.nanoTime();
-			pool.recycle(byteBufer);	
-			//System.out.println("recycle usage "+(System.nanoTime()-start));
-		}
-		long used=(System.currentTimeMillis()-start);
-		System.out.println("total used time  "+used+ " avg speed "+allocTimes/used);
-	}
+    @Test
+    public void testAllocate() {
+        int pageSize = 1024 * 1024 * 100;
+        int allocTimes = 1024 * 1024 * 100;
+        DirectByteBufferPool pool = new DirectByteBufferPool(pageSize, (short) 256, (short) 8);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < allocTimes; i++) {
+            //System.out.println("allocate "+i);
+            //long start=System.nanoTime();
+            int size = i % (1024 * 8) + 1;
+            ByteBuffer byteBufer = pool.allocate(size);
+            //System.out.println("alloc "+size+" usage "+(System.nanoTime()-start));
+            //start=System.nanoTime();
+            pool.recycle(byteBufer);
+            //System.out.println("recycle usage "+(System.nanoTime()-start));
+        }
+        long used = (System.currentTimeMillis() - start);
+        System.out.println("total used time  " + used + " avg speed " + allocTimes / used);
+    }
+
+    @Test
+    public void testAllocateWithDifferentAddress() {
+        int size = 256;
+        int pageSize = size * 4;
+        int allocTimes = 8;
+        DirectByteBufferPool pool = new DirectByteBufferPool(pageSize, (short) 256, (short) 2);
+
+        Map<Long, ByteBuffer> buffs = new HashMap<Long, ByteBuffer>(8);
+        ByteBuffer byteBuffer = null;
+        DirectBuffer directBuffer = null;
+        ByteBuffer temp = null;
+        long address;
+        boolean failure = false;
+        for (int i = 0; i < allocTimes; i++) {
+            byteBuffer = pool.allocate(size);
+            if (byteBuffer == null) {
+                Assert.fail("Should have enough memory");
+            }
+            directBuffer = (DirectBuffer) byteBuffer;
+            address = directBuffer.address();
+            System.out.println(address);
+            temp = buffs.get(address);
+            buffs.put(address, byteBuffer);
+            if (null != temp) {
+                failure = true;
+                break;
+            }
+        }
+
+        for (ByteBuffer buff : buffs.values()) {
+            pool.recycle(buff);
+        }
+
+        if (failure == true) {
+            Assert.fail("Allocate with same address");
+        }
+    }
+
+    @Test
+    public void testAllocateNullWhenOutOfMemory() {
+        int size = 256;
+        int pageSize = size * 4;
+        int allocTimes = 9;
+        DirectByteBufferPool pool = new DirectByteBufferPool(pageSize, (short) 256, (short) 2);
+        long start = System.currentTimeMillis();
+        ByteBuffer byteBuffer = null;
+        List<ByteBuffer> buffs = new ArrayList<ByteBuffer>();
+        int i = 0;
+        for (; i < allocTimes; i++) {
+            byteBuffer = pool.allocate(size);
+            if (byteBuffer == null) {
+                break;
+            }
+            buffs.add(byteBuffer);
+        }
+        for (ByteBuffer buff : buffs) {
+            pool.recycle(buff);
+        }
+
+        Assert.assertEquals("Should out of memory when i = " + 8, i, 8);
+    }
+
+    @Test
+    public void testAllocateSign() {
+        int size = 256;
+        int pageSize = size * 4;
+        int allocTimes = 9;
+        DirectByteBufferPool pool = new DirectByteBufferPool(pageSize, (short) 256, (short) 2);
+        long start = System.currentTimeMillis();
+        ByteBuffer byteBuffer = null;
+        List<ByteBuffer> buffs = new ArrayList<ByteBuffer>();
+        int i = 0;
+        for (; i < allocTimes; i++) {
+            byteBuffer = pool.allocate(size);
+            if (byteBuffer == null) {
+                break;
+            }
+            buffs.add(byteBuffer);
+        }
+        for (ByteBuffer buff : buffs) {
+            pool.recycle(buff);
+        }
+
+        Assert.assertEquals("Should out of memory when i = " + 8, i, 8);
+    }
 }
